eee6ed3f485b73750f6923fc21371a9c1530ca50
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-10-13 11:17:00 +0200

    heritage

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Customer, type: :model do
   it 'Create a customer' do
     customer = create(:customer)
 
-    expect(customer.name).to start_with('M.')
+    expect(customer.full_name).to start_with('M.')
+  end
+
+  it '#vip' do
+    customer = create(:customer_vip)
+
+    expect(customer.vip).to be_truthy
   end
 end
