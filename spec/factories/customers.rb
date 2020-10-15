b975bc736846678b382ef5d2ed4e35483a0e4e79
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-10-15 09:11:48 +0200

    Association has_many

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :customer do
     transient do
       upcased { false }
+      qtt_orders { 3 }
     end
     name { Faker::Name.name }
     sequence(:email, 35) { |n| "mon_email-#{n}@email.com" }
@@ -22,6 +23,12 @@ FactoryBot.define do
     trait :default do
       vip { false }
       days_to_pay { 15 }
+    end
+
+    trait :with_orders do
+      after(:create) do |customer, evaluator|
+        create_list(:order, evaluator.qtt_orders, customer: customer)
+      end
     end
 
     factory :customer_male, traits: [:male]
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'Il y a une commande' do
+  it 'Belongs_to' do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
 
-  it '3 commandes' do
+  it '3 commandes - Create_list' do
     orders = create_list(:order, 3, description: 'Tested')
-    p orders
     expect(orders.count).to eq(3)
+  end
+
+  it 'has_many' do
+    customer = create(:customer, :with_orders, qtt_orders: 5)
+    p customer.orders
+    expect(customer.orders.count).to eq(5)
   end
 end
