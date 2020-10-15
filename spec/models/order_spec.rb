b9593fd89580f5632e8e2d6e60815ff0e23dfb7d
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-10-15 09:01:37 +0200

    create list

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Order, type: :model do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it '3 commandes' do
+    orders = create_list(:order, 3, description: 'Tested')
+    p orders
+    expect(orders.count).to eq(3)
+  end
 end
